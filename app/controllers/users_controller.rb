6766f2bc894e164a978c52aca9bd1e00092a7c12
LetsJessCode <letsjesscode@gmail.com>
2020-08-15 21:02:12 -0400

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to students_path(current_user)
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
+    has_secure_password
 end
